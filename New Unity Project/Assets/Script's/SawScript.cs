39e0675a6ca0e5b04806fe58f9a57436f7b76ce6
Sheshesi <alexkadyrbayev@gmail.com>
2021-03-12 22:36:18 +0700

    Added first scene

---
+++ New Unity Project/Assets/Script's/SawScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawScript : MonoBehaviour
+{
+	public float speed = 100f;
+
+	void Update()
+	{
+		transform.Rotate(new Vector3(0f, 0f, speed * Time.deltaTime));
+	}
+}
